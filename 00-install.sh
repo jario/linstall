2d84fa678eb8639332953c078822328599dac534
Jario <jario@outlook.com>
2026-08-08 10:00:28 -0300

    Add installation script for Helium browser

---
+++ 00-install.sh
@@ -0,0 +1,6 @@
+#!
+
+# helium browser
+curl -fsSL https://raw.githubusercontent.com/imputnet/helium-linux/main/pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/helium.gpg
+echo "deb [signed-by=/usr/share/keyrings/helium.gpg] https://pkg.helium.computer/deb stable main" | sudo tee /etc/apt/sources.list.d/helium.list
+sudo apt update && sudo apt install helium-bin
