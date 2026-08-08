0470a4cf890cff4819825f23b25f43c1c45a0378
Jario <jario@outlook.com>
2026-08-08 10:03:32 -0300

    Add installation commands for Brave browser

--- 00-install.sh
+++ 00-install.sh
@@ -4,3 +4,12 @@
 curl -fsSL https://raw.githubusercontent.com/imputnet/helium-linux/main/pubkey.asc | sudo gpg --dearmor -o /usr/share/keyrings/helium.gpg
 echo "deb [signed-by=/usr/share/keyrings/helium.gpg] https://pkg.helium.computer/deb stable main" | sudo tee /etc/apt/sources.list.d/helium.list
 sudo apt update && sudo apt install helium-bin
+
+
+# brave browser
+curl -fsS https://dl.brave.com/install.sh | sh
+
+# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+# sudo apt update
+# sudo apt install brave-browser
